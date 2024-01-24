b9677cb89c25af236f4d0203e3b03ddc1dbb9a5b
Dan Gamble <dan@dangamble.co.uk>
2024-01-24 13:58:35 +0000

    Use Shopify API codegen preset

---
+++ .graphqlrc.ts
@@ -0,0 +1,53 @@
+import fs from "fs";
+
+import { LATEST_API_VERSION } from "@shopify/shopify-api";
+import { shopifyApiProject, ApiType } from "@shopify/api-codegen-preset";
+import type { IGraphQLConfig } from "graphql-config";
+
+const outputDir = './app/types'
+const defaultConfig = shopifyApiProject({
+  apiType: ApiType.Admin,
+  apiVersion: LATEST_API_VERSION,
+  documents: ['./app/**/*.{js,ts,jsx,tsx}'],
+  outputDir: outputDir,
+})
+
+defaultConfig.extensions.codegen.generates[`${outputDir}/admin.types.ts`] =
+  defaultConfig.extensions.codegen.generates[`${outputDir}/admin.types.d.ts`]
+delete defaultConfig.extensions.codegen.generates[
+  `${outputDir}/admin.types.d.ts`
+  ]
+
+function getConfig() {
+  const config: IGraphQLConfig = {
+    projects: {
+      default: defaultConfig,
+    },
+  };
+
+  let extensions: string[] = [];
+  try {
+    extensions = fs.readdirSync("./extensions");
+  } catch {
+    // ignore if no extensions
+  }
+
+  for (const entry of extensions) {
+    const extensionPath = `./extensions/${entry}`;
+    const schema = `${extensionPath}/schema.graphql`;
+    if (!fs.existsSync(schema)) {
+      continue;
+    }
+    config.projects[entry] = {
+      schema,
+      documents: [`${extensionPath}/**/*.graphql`],
+    };
+  }
+
+  // @ts-ignore
+  config.ignoreNoDocuments = true
+
+  return config;
+}
+
+module.exports = getConfig();
