ea62049469cb5e7433125bf747c16c8b63454292
Dan Gamble <dan@dangamble.co.uk>
2024-01-24 13:24:19 +0000

    Various package updates

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,23 @@
+import fs from 'fs'
+
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
-// if (
-//   process.env.HOST &&
-//   (!process.env.SHOPIFY_APP_URL ||
-//     process.env.SHOPIFY_APP_URL === process.env.HOST)
-// ) {
-//   process.env.SHOPIFY_APP_URL = process.env.HOST;
-//   delete process.env.HOST;
+if (
+  process.env.HOST &&
+  (!process.env.SHOPIFY_APP_URL ||
+    process.env.SHOPIFY_APP_URL === process.env.HOST)
+) {
+  process.env.SHOPIFY_APP_URL = process.env.HOST;
+  delete process.env.HOST;
+}
+
+// From: https://codefrontend.com/deploy-shopify-apps-on-cloudflare/
+// if (process.env.NODE_ENV === "development") {
+//   fs.writeFileSync(
+//     ".dev.vars",
+//     `SHOPIFY_APP_URL=${process.env.SHOPIFY_APP_URL}\nSHOPIFY_API_KEY=${process.env.SHOPIFY_API_KEY}\nSHOPIFY_API_SECRET=${process.env.SHOPIFY_API_SECRET}\nSCOPES=${process.env.SCOPES}\n`,
+//   );
 // }
 
 /** @type {import('@remix-run/dev').AppConfig} */
