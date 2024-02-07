185b8d2a69961b3571693685d73c4964d5cd3da6
Dan Gamble <dan@dangamble.co.uk>
2024-02-07 11:58:01 +0000

    Add checkout profile selector

--- app/routes/app.branding._index/route.tsx
+++ app/routes/app.branding._index/route.tsx
@@ -1,7 +1,7 @@
-import type { ActionFunctionArgs, LinksFunction } from '@remix-run/cloudflare';
+import type { ActionFunctionArgs, LinksFunction } from '@remix-run/cloudflare'
 import { json } from '@remix-run/cloudflare'
-import { useFetcher, useNavigation, useSubmit } from '@remix-run/react'
-import { BlockStack, Card, Layout, Page, PageActions, Text } from '@shopify/polaris'
+import { useFetcher, useNavigation, useSearchParams, useSubmit } from '@remix-run/react'
+import { BlockStack, Card, Layout, Page, PageActions, Select, Text } from '@shopify/polaris'
 import { TextFontIcon } from '@shopify/polaris-icons'
 import { TextField } from '~/components/text-field'
 import { useCheckoutBranding, useCheckoutBrandingData } from '~/routes/app.branding/route'
@@ -13,7 +13,7 @@ import { checkoutBrandingFormData } from '~/routes/app.branding._index/schema'
 import { parseWithZod } from '@conform-to/zod'
 import { makeRequest } from '~/utils/graphql.server'
 import {
-  checkoutBrandingUpsertMutation
+  checkoutBrandingUpsertMutation,
 } from '~/routes/app.branding._index/graphql/mutations/checkout-branding-upsert-mutation'
 
 export const links: LinksFunction = () => [
@@ -37,7 +37,7 @@ export async function action ({ request }: ActionFunctionArgs) {
     variables: {
       checkoutProfileId: submission.value.checkoutProfileId,
       checkoutBrandingInput: submission.value.checkoutBrandingInput,
-    }
+    },
   })
 
   return json({ status: 'success', submission } as const)
@@ -46,6 +46,7 @@ export async function action ({ request }: ActionFunctionArgs) {
 export default function CheckoutBranding () {
   const fetcher = useFetcher()
   const navigation = useNavigation()
+  const [, setSearchParams] = useSearchParams()
   const submit = useSubmit()
 
   const checkoutBranding = useCheckoutBranding()
@@ -89,80 +90,115 @@ export default function CheckoutBranding () {
         </Layout.Section>
 
         <Layout.Section variant="oneThird">
-          <Card padding="400">
-            <BlockStack gap="200">
-              <Text variant="headingSm" as="h2">
-                Corner radiuses
-              </Text>
+          <BlockStack gap="400">
+            {checkoutBrandingData.branding.profiles.length > 1 && (
+              <Card padding="400">
+                <BlockStack gap="200">
+                  <Text variant="headingSm" as="h2">
+                    Checkout profiles
+                  </Text>
 
-              <BlockStack gap="400">
-                <Text as="p">
-                  This property group applies to global corner radius token values.
-                </Text>
-
-                <div>
                   <BlockStack gap="400">
-                    <TextField
-                      name="base"
-                      control={checkoutBranding.forms.cornerRadiusForm.control}
-                      label="Base"
-                      type="number"
-                      min="0"
-                      autoComplete="off"
-                      helpText="The pixel value for small corner radiuses. It should be strictly positive."
-                      onChange={(value, field) => {
-                        const number = parseInt(value, 10)
+                    <Select
+                      label="Checkout profiles"
+                      labelHidden
+                      options={[
+                        ...checkoutBrandingData.branding.profiles.map(profile => (
+                          {
+                            label: profile.name,
+                            value: profile.id,
+                          }
+                        )),
+                      ]}
+                      value={checkoutBrandingData.branding.profileId}
+                      onChange={value => {
+                        setSearchParams(previousParams => {
+                          previousParams.set('profileId', value)
 
-                        if (Number.isNaN(number)) {
-                          return field.onChange(null)
-                        }
-
-                        return field.onChange(number)
-                      }}
-                    />
-
-                    <TextField
-                      name="small"
-                      control={checkoutBranding.forms.cornerRadiusForm.control}
-                      label="Small"
-                      type="number"
-                      min="0"
-                      autoComplete="off"
-                      helpText="The pixel value for small corner radiuses. It should be strictly positive."
-                      onChange={(value, field) => {
-                        const number = parseInt(value, 10)
-
-                        if (Number.isNaN(number)) {
-                          return field.onChange(null)
-                        }
-
-                        return field.onChange(number)
-                      }}
-                    />
-
-                    <TextField
-                      name="large"
-                      control={checkoutBranding.forms.cornerRadiusForm.control}
-                      label="Large"
-                      type="number"
-                      min="0"
-                      autoComplete="off"
-                      helpText="The pixel value for large corner radiuses. It should be strictly positive."
-                      onChange={(value, field) => {
-                        const number = parseInt(value, 10)
-
-                        if (Number.isNaN(number)) {
-                          return field.onChange(null)
-                        }
-
-                        return field.onChange(number)
+                          return previousParams
+                        })
                       }}
                     />
                   </BlockStack>
-                </div>
+                </BlockStack>
+              </Card>
+            )}
+
+            <Card padding="400">
+              <BlockStack gap="200">
+                <Text variant="headingSm" as="h2">
+                  Corner radiuses
+                </Text>
+
+                <BlockStack gap="400">
+                  <Text as="p">
+                    This property group applies to global corner radius token values.
+                  </Text>
+
+                  <div>
+                    <BlockStack gap="400">
+                      <TextField
+                        name="base"
+                        control={checkoutBranding.forms.cornerRadiusForm.control}
+                        label="Base"
+                        type="number"
+                        min="0"
+                        autoComplete="off"
+                        helpText="The pixel value for small corner radiuses. It should be strictly positive."
+                        onChange={(value, field) => {
+                          const number = parseInt(value, 10)
+
+                          if (Number.isNaN(number)) {
+                            return field.onChange(null)
+                          }
+
+                          return field.onChange(number)
+                        }}
+                      />
+
+                      <TextField
+                        name="small"
+                        control={checkoutBranding.forms.cornerRadiusForm.control}
+                        label="Small"
+                        type="number"
+                        min="0"
+                        autoComplete="off"
+                        helpText="The pixel value for small corner radiuses. It should be strictly positive."
+                        onChange={(value, field) => {
+                          const number = parseInt(value, 10)
+
+                          if (Number.isNaN(number)) {
+                            return field.onChange(null)
+                          }
+
+                          return field.onChange(number)
+                        }}
+                      />
+
+                      <TextField
+                        name="large"
+                        control={checkoutBranding.forms.cornerRadiusForm.control}
+                        label="Large"
+                        type="number"
+                        min="0"
+                        autoComplete="off"
+                        helpText="The pixel value for large corner radiuses. It should be strictly positive."
+                        onChange={(value, field) => {
+                          const number = parseInt(value, 10)
+
+                          if (Number.isNaN(number)) {
+                            return field.onChange(null)
+                          }
+
+                          return field.onChange(number)
+                        }}
+                      />
+                    </BlockStack>
+                  </div>
+                </BlockStack>
               </BlockStack>
-            </BlockStack>
-          </Card>
+            </Card>
+          </BlockStack>
         </Layout.Section>
 
         <Layout.Section variant="fullWidth">
